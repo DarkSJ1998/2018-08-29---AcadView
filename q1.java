3cf644b3559755800414fad264ff29daab3fa03c
DarkSJ1998 <sarthakjn12@gmail.com>
2018-09-05 20:58:25 +0530

    Add q1.java

---
+++ q1.java
@@ -0,0 +1,19 @@
+class q1
+{
+	public static void main(String[] x)
+	{
+		String str = "Hello World!";
+		String rev = "";
+		char ch;
+		int i,len = str.length();
+		for(i=0;i<len;i++)
+		{
+			ch = str.charAt(len-i-1);
+			rev = rev.concat(""+ch);
+		}
+		
+		System.out.println("String : \"" + str + "\"");
+		System.out.println("Reverse = \"" + rev + "\"");
+		System.out.println();
+	}
+}
